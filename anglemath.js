63c058411b1d898ac17ac7b54cd2f82bb15e58b9
Philip Peterson <im@starryexpanse.com>
2015-04-09 04:06:35 -0400

    Initial commit

---
+++ anglemath.js
@@ -0,0 +1,46 @@
+var PI = Math.PI;
+var TWO_PI = 2*PI;
+
+/**
+ * Computes "b-a" in angular terms, i.e. the smallest
+ * (in terms of magnitude) angle x such that a+x = b.
+ *
+ * This may also be called the angular difference or
+ * angular distance.
+ */
+function difference(b, a) {
+   // First normalize each arg
+   a = normalize(a);
+   b = normalize(b);
+
+   var diff = b-a;
+   var mag = Math.abs(b-a);
+
+   var answer = b-a;
+   if (mag < PI) {
+      return answer;
+   }
+   else {
+      if (answer > 0) {
+         return TWO_PI-answer;
+      }
+      else {
+         return TWO_PI+answer
+      }
+   }
+}
+
+/**
+ * Converts an angle to the range [0, 2*PI)
+ */
+function normalize(theta) {
+   if (theta >= 0) {
+      return theta % TWO_PI;
+   }
+   else {
+      return (theta % TWO_PI) + TWO_PI;
+   }
+}
+
+exports.normalize = normalize;
+exports.difference = difference;
